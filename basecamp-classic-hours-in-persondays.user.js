89268e775034c1bba97b2c9543f23ec0178c53e3
thomas.kirchhofer <thomas_kirchhofer@bluewin.ch>
2017-08-24 15:28:16 +0200

    Ausrechnen der Zeit in Tagen bei jedem todo.

--- basecamp-classic-hours-in-persondays.user.js
+++ basecamp-classic-hours-in-persondays.user.js
@@ -3,15 +3,44 @@
 // @namespace   soultank.ch
 // @include     https://*.basecamphq.com/*
 // @version     1
-// @grant       none
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant       GM_log
 // ==/UserScript==
+
+
+console.log("test");
+
+//Diese Funktion zeigt neben den Stunden die Zeit in Tagen an
+//https://stackoverflow.com/questions/8281441/fire-greasemonkey-script-on-ajax-request/8283815#8283815
+function calculatePersonDaysForCurrentTodo (jNode) {
+
+    //***** Hier wird dann aus den Stunden Tage berechnet *****
+    var hoursInPersonDay = 8;
+    var targetDiv = document.getElementsByClassName("total").item(0);
+    var taskHours = targetDiv.firstElementChild.innerHTML;
+    var hoursString = taskHours.replace(',', '.');
+    var hoursWorked = parseFloat(hoursString);
+    var daysWorked = hoursString / hoursInPersonDay;
+    var daysWorkedTwoDecimals = daysWorked.toFixed(2);
+    var textDaysTotal = ("(" + daysWorkedTwoDecimals.toString() + " Tage)");
+    var daysWorkedText = document.createElement("A");
+    var textnode = document.createTextNode(textDaysTotal);
+    daysWorkedText.appendChild(textnode);
+    targetDiv.insertBefore(textnode, targetDiv.childNodes[3]);
+    //
+}
+
+//Diese Funktion wartet darauf, dass ein div mit class "item_time_tracker" erscheint und ruft dann calculatePersonDaysForCurrentTodo auf
+waitForKeyElements ("div.item_time_tracker", calculatePersonDaysForCurrentTodo);
+
 
 //configuration: how many hours does a person day have?
 var hoursInPersonDay = 8;
 
 
-
-var hoursWorkedCell = document.getElementById('hours_subtotal')
+var hoursWorkedCell = document.getElementById('hours_subtotal');
+console.log("hoursWorkedCell="+ hoursWorkedCell.innerHTML);
 var hoursWorkedString = hoursWorkedCell.innerHTML.replace(',', '.');
 console.log("hoursWorkedString: "+ hoursWorkedString);
 var hoursWorked = parseFloat(hoursWorkedString);
@@ -39,4 +68,3 @@ daysWorkedCell.setAttribute("class","hours");
 var nextSiblingOfHoursWorkedCell = hoursWorkedCell.nextSibling;
 
 hoursWorkedCell.parentNode.insertBefore(daysWorkedCell, nextSiblingOfHoursWorkedCell);
-
